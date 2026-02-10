7d672af3cfae4b1fb687ebf2061933a56babdf72
PaoloMucedero <p.mucedero@gmail.com>
2026-02-10 08:40:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require(`express`)
+const app = express()
+const port = 3000
+// Collegamento file responsabile delle rotte
+const postsRouter = require("./routers/posts");
+
+// attivazione cartella public
+app.use(express.static("public"));
+// devio gestione richieste su file routers/posts.js
+app.use("/posts", postsRouter);
+// Routing tramite express - vedi file posts.js
+// HOME PAGE
+app.get("/", (req, res) => {
+    res.send("<h1>Home Route app Blog</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+
+})
---
+++ routers/posts.js
@@ -0,0 +1,54 @@
+// QUESTO FILE GESTISCE TUTTE LE ROTTE DELLA MIA APP
+const express = require("express");
+const router = express.Router();
+
+// UTILIZZO IL FILE CHE CONTIENE L'ARRAY DI OGGETTI
+const postsList = require(`./../data/posts`)
+
+// INDEX → lista di tutti i post
+router.get("/", (req, res) => {
+  // provo a filtrare la lista di post
+
+  // devo creare una variabile che parte dallo stesso array
+  let filteredList = postsList;
+  // scrivo la logica del filtro
+  if (req.query.title) {
+    filteredList = postsList.filter(
+      post => post.title.includes(req.query.title)
+    );
+  }
+
+
+
+  const oggettoListaPost = {
+    numeroPost: postsList.length,
+    listaPost: filteredList
+  };
+  // test funzionamento filtro
+  res.json(oggettoListaPost)
+  //res.send("Lista dei post");
+});
+
+// SHOW → dettaglio di un singolo post
+router.get("/:id", (req, res) => {
+  res.send(`Dettaglio del post ${req.params.id}`);
+});
+
+// CREATE → creazione di un nuovo post
+router.post("/", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
+
+// UPDATE → modifica di un post esistente
+router.put("/:id", (req, res) => {
+  res.send(`Modifica del post ${req.params.id}`);
+});
+
+// DELETE → cancellazione di un post
+router.delete("/:id", (req, res) => {
+  res.send(`Cancellazione del post ${req.params.id}`);
+});
+
+
+
+module.exports = router;
